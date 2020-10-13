2bbffcc2cb6f2b3cc57a781b819578ba994c8e11
Fabian <fabianperez592@gmail.com>
2020-10-13 13:43:41 -0300

    Update shouldCompress.js
    Compresión minima en .png y .gifs 5KB

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -1,5 +1,5 @@
 const MIN_COMPRESS_LENGTH = 1024
-const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 20
+const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 5
 
 function shouldCompress(req) {
   const { originType, originSize, webp } = req.params
